13deea36388e7943c560adcede808ff308233ae0
tarde <tarde@A13E41>
2024-01-11 20:47:48 +0100

    falta todo

---
+++ 28_cursos_web_bd/src/main/java/model/Usuario.java
@@ -0,0 +1,29 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+	private String usuario;
+	private String password;
+	public Usuario(String usuario, String password) {
+		super();
+		this.usuario = usuario;
+		this.password = password;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
